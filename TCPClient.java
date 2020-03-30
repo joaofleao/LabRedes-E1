95abb5d35a4a8a8eab69414c8de39becdbaf7a84
joaofleao <joaofleao@gmail.com>
2020-03-30 09:29:54 -0300

    tcp pronto e funcional

--- TCPClient.java
+++ TCPClient.java
@@ -2,30 +2,32 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-class TCPClient {
-    public static void main(String argv[]) throws Exception {
-        // cria o stream do teclado e le
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        // name = "test.txt";
-        String name = inFromUser.readLine();
+public class TCPClient {
+	public static void main(String[] args) throws Exception {
+		//pegar nome do arquivo
+		Scanner tc = new Scanner(System.in);
+		System.out.println("Digite o nome do arquivo:");
+		String fName = tc.nextLine();
 
-        FileInputStream file = new FileInputStream(name);
+		//abrir arquivo
+		File f = new File(fName);
+		FileInputStream in = new FileInputStream(f);
 
-        // declara socket cliente
-        Socket clientSocket = new Socket("localhost", 1971);
+		//criar socket do cliente
+		Socket clientSocket = new Socket("localhost", 1971);
 
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		//definir como enviar informacoes
+		DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
 
-        outToServer.writeBytes(name+"\n");
-        outToServer.flush();
-        int teste = 0;
-        int by;
-        while((by = file.read()) != -1) {
-            outToServer.write(by);
-            //System.out.println(teste++ + ": " + by);
-        }
+		//enviar nome do arquivo para o server
+		out.writeBytes(f.getName()+"\n");
 
-         // fecha o cliente
-        clientSocket.close();
-    }
+		//mandar texto para o server
+		//ler arquivo todo char por char
+		for (int c = in.read(); c != -1; c = in.read() ) {
+			//enviar o char 
+			out.write(c);
+		}
+		System.out.println("Arquivo enviado");
+	}
 }
--- TCPServer.java
+++ TCPServer.java
@@ -1,29 +1,35 @@
 import java.io.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.*;
 
-class TCPServer {
-    public static void main(String argv[]) throws Exception {
-        // cria socket do servidor com a porta 1971
-        ServerSocket welcomeSocket = new ServerSocket(1971);
-        Socket connectionSocket = welcomeSocket.accept();
-        InputStream in = connectionSocket.getInputStream();
-        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(in));
-        String fName = inFromClient.readLine();
 
-        File f1 = new File("out_"+fName);
+public class TCPServer {
+    public static void main(String[] args) {
+        try {
+            //iniciar socket de servidor e aceitar clientes
+            ServerSocket serverSocket = new ServerSocket(1971);
+            Socket clientSocket = serverSocket.accept();
 
-        FileOutputStream out = new FileOutputStream(f1);
-        int c;
-        while ((c = in.read()) != -1) {
-            out.write(c);
-        }
+
+            //ler dados do cliente
+            InputStream in = clientSocket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(in);
+            BufferedReader reader = new BufferedReader(isr);
+            
+
+            //pegar nome do arquivo
+            String fName = reader.readLine();
+
+            //adicionar out para ficar mais entendivel
+            File f1 = new File("out_"+fName);
+
+            //definir dados que serao salvos no arquivo
+            FileOutputStream out = new FileOutputStream(f1);
+           
+            //ler texto char por char
+            for (int c = in.read(); c != -1; c = in.read() ) {
+                //adicionar o char no arquivo
+                out.write(c);
+            }
+            } catch (IOException e) {}
     }
 }
